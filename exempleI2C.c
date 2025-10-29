351b5101e463c1a7033b9e15204186d5659a5b5b
kevin cotton <kcurcot@gmail.com>
2025-10-28 22:58:22 -0400

    Add files via upload

---
+++ exempleI2C.c
@@ -0,0 +1,65 @@
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h> //for IOCTL defs
+#include <fcntl.h>
+
+#define I2C_BUS "/dev/i2c-0" // fichier Linux representant le BUS #0
+//#define I2C_BUS "/dev/i2c-1" // fichier Linux representant le BUS #1
+
+/*******************************************************************************************/
+/*******************************************************************************************/
+#define CAPTEUR_I2C_ADDRESS 0xff	// adresse I2C du capteur de distance -- A REMPLACER
+#define CAPTEUR_REGID 0xffff	// adresse du registre ID du capteur de distance -- A REMPLACER
+/*******************************************************************************************/
+/*******************************************************************************************/
+
+int Lire_ID_Capteur(int)
+
+int main()
+{
+	int fdPortI2C;  // file descriptor I2C
+	
+	fdPortI2C = open(I2C_BUS, O_RDWR); // ouverture du 'fichier', création d'un 'file descriptor' vers le port I2C
+	if(fdPortI2C == -1)
+	{
+		perror("erreur: ouverture du port I2C ");
+		return 1;
+	}
+	
+	/// Liaison de l'adresse I2C au fichier (file descriptor) du bus I2C et Initialisation
+	if(ioctl(fdPortI2C, I2C_SLAVE_FORCE, adresse_I2C) < 0)
+	{ 	// I2C_SLAVE_FORCE if it is already in use by a driver (i2cdetect : UU)
+		perror("erreur: adresse du device I2C ");
+		close(fdPortI2C);
+		return 1;
+	}
+
+	printf("ID CAPTEUR = %#04X\n", Lire_ID_Capteur(fdPortI2C));
+
+	close(fdPortI2C);
+	return 0;
+}
+
+int Lire_ID_Capteur(int fdPortI2C)
+{
+	uint8_t Identification; 		// emplacement memoire pour stocker la donnee lue
+	uint8_t Registre16bit[2];
+	
+	Registre16bit[0] = (uint8_t)(CAPTEUR_REGID >> 8);
+	Registre16bit[1] = (uint8_t)CAPTEUR_REGID;
+	
+	if (write(fdPortI2C, Registre16bit, 2) != 2)
+	{
+		perror("erreur: I2C_ecrire ");
+		return -1;
+	}
+	if (read(fdPortI2C, &Identification, 1) != 1)
+	{
+		perror("erreur: I2C_Lire ");
+		return -1;
+	}
+	
+	return Identification;
+}
